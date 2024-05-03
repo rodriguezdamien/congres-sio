bc95625e740c3efe64460a613a695bcb3b334a32
Damien R. <damien@damienrodriguez.fr>
2024-05-03 10:44:14 +0200

    Modification du fichier exemple

--- congres.dll/Managers/DBManager - Exemple.cs
+++ congres.dll/Managers/DBManager - Exemple.cs
@@ -8,7 +8,7 @@ namespace congres.dll
     public static class DBManager
     {
         //METTRE LES INFORMATIONS DE CONNEXION ICI
-        static private string dataSource = "ARIA-LITE";
+        static private string dataSource = "Nom de PC";
         static private string login = "AppliGestCongres";
         static private string password = "MdpComplexe34";
         static public SqlConnection ConnexionDB = new SqlConnection($"Data Source={DBManager.dataSource};Initial Catalog=CongresDB;Integrated Security=True;user Id={DBManager.login};password={DBManager.password}");
