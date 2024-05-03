38e244141495b73fb57563d68e0266bf25e89b63
Damien R. <damien@damienrodriguez.fr>
2024-05-03 09:47:04 +0200

    add Ligue manager and test

---
+++ Congres.Test/ManagerTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using congres.dll;
+using congres.dll.Managers;
+
+namespace Congres.Test
+{
+    [TestClass]
+    public class ManagerTest
+    {
+        [TestMethod]
+        public void TestGetLigues()
+        {
+            List<Ligue> ligues = LigueManager.GetLigues();
+            Assert.IsFalse(ligues.Count == 0);
+        }
+    }
+}
---
+++ congres.dll/Managers/LigueManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace congres.dll.Managers
+{
+    public static class LigueManager
+    {
+        public static List<Ligue> GetLigues()
+        {
+            List<Ligue> ligues = new List<Ligue>();
+            try
+            {
+                DBManager.ConnexionDB.Open();
+                SqlCommand req = new SqlCommand("SELECT id,nomLigue,adresse,cp,ville FROM Ligue", DBManager.ConnexionDB);
+                SqlDataReader reader = req.ExecuteReader();
+                while (reader.Read())
+                {
+                    ligues.Add(new Ligue(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4)));
+                }
+            }
+            finally
+            {
+                DBManager.ConnexionDB.Close();
+            }
+            return ligues;
+        }
+    }
+}
