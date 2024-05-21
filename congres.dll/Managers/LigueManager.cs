076c9e4c2d955cc848360f75f6d2cb2e1f393d67
nemuoM <abdelmoumen.morabet@gmail.com>
2024-05-21 14:26:52 +0200

    début du travail sur la classe LigueManager

--- congres.dll/Managers/LigueManager.cs
+++ congres.dll/Managers/LigueManager.cs
@@ -12,14 +12,20 @@ namespace congres.dll.Managers
         public static List<Ligue> GetLigues()
         {
             List<Ligue> ligues = new List<Ligue>();
+
+            SqlCommand reqGet = new SqlCommand("SELECT id,nomLigue,adresse,cp,ville FROM Ligue", DBManager.ConnexionDB);
+
             try
             {
                 DBManager.ConnexionDB.Open();
-                SqlCommand req = new SqlCommand("SELECT id,nomLigue,adresse,cp,ville FROM Ligue", DBManager.ConnexionDB);
-                SqlDataReader reader = req.ExecuteReader();
+                SqlDataReader reader = reqGet.ExecuteReader();
                 while (reader.Read())
                 {
-                    ligues.Add(new Ligue(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4)));
+                    ligues.Add(new Ligue(id: reader.GetInt32(0), 
+                                         nomLigue: reader.GetString(1), 
+                                         adresse: reader.GetString(2), 
+                                         codePostal: reader.GetString(3), 
+                                         ville: reader.GetString(4)));
                 }
             }
             finally
@@ -27,6 +33,30 @@ namespace congres.dll.Managers
                 DBManager.ConnexionDB.Close();
             }
             return ligues;
+        }
+
+        public static bool UpdtLigue(Ligue ligue)
+        {
+            bool isUdpt = false;
+
+            SqlCommand reqUpdt = new SqlCommand("UPDATE LIGUE SET nomLigue = @nomL, adresse = @a, cp = @cp, ville = @v WHERE id = @idL", DBManager.ConnexionDB);
+            reqUpdt.Parameters.AddWithValue("@nomL", ligue.Nom);
+            reqUpdt.Parameters.AddWithValue("@a", ligue.Adresse);
+            reqUpdt.Parameters.AddWithValue("@cp", ligue.CodePostal);
+            reqUpdt.Parameters.AddWithValue("@v", ligue.Ville);
+            reqUpdt.Parameters.AddWithValue("@idL", ligue.Id);
+
+            try
+            {
+                DBManager.ConnexionDB.Open();
+                if (reqUpdt.ExecuteNonQuery() > 0) isUdpt = true;
+            }
+            finally
+            {
+                DBManager.ConnexionDB.Close();
+            }
+
+            return isUdpt;
         }
     }
 }
