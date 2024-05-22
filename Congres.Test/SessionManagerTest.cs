501d557083a54e210c9eba306ed7e646df94c8ad
Damien R. <damien@damienrodriguez.fr>
2024-05-22 17:26:00 +0200

    Ajout d'un test unitaire pour GetCongressistesNonParticipants

---
+++ Congres.Test/SessionManagerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using congres.dll;
+using congres.dll.Managers;
+
+namespace Congres.Test
+{
+    [TestClass]
+    public class SessionManagerTest
+    {
+        /// <summary>
+        /// Test de la présence d'un congressiste participant à une session dans la fonction GetCongressistesNonParticipants
+        /// Si le participant est présent dans la liste, le test échoue.
+        /// </summary>
+        [TestMethod]
+        public void TestCongressisteNonParticipants()
+        {
+            Session uneSession = SessionManager.GetSessions()[1];
+            List<Congressiste> congressistes = SessionManager.GetCongressistesNonParticipants(uneSession);
+            bool found = false;
+            foreach(Congressiste c in congressistes)
+            {
+                if(c.Id == 1)
+                    found = true;
+            }
+            Assert.IsFalse(found);
+            
+        }
+    }
+}
